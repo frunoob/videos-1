4bf0b20055b8f41731573187561ff0438b216d71
frunoob <3024446880@qq.com>
2020-12-12 04:59:36 +0800

    20201212 45935

---
+++ auto.bat
@@ -0,0 +1,1 @@
+./ffmpeg.exe -y -i videos.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb videos.ts && ./ffmpeg.exe -i videos.ts -c copy -map 0 -f segment -segment_list playlist.m3u8 -segment_time 5 videos%03d.ts
